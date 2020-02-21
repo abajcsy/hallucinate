644883892033cd9a68567bbb07daf5042a25a504
abajcsy <andrea.victoria.b@gmail.com>
2020-02-20 16:07:47 -0800

    plotting of state-dependent controls works

--- matlab/plotting_scripts/boltzmann_plot_scratch.m
+++ matlab/plotting_scripts/boltzmann_plot_scratch.m
@@ -15,7 +15,7 @@ ind = 1;
 figure;
 
 %% Setup variables
-x = g.xs; 
+x = g.xs;
 
 lb = -pi+1e-2;
 ub = pi;
@@ -23,11 +23,14 @@ v = 0.6;
 numCtrls = 100;
 inc = (ub-lb)/(numCtrls-1);
 deltaT = 1;
-theta = [0 0];
+theta = [1 1];
 
 num_plots = 5;
 
 ctrls = linspace(lb, ub, num_plots);
+
+% Get a 2D grid structure for just x and y values.
+grid2D = createGrid(grid_min(1:2), grid_max(1:2), N(1:2));
 
 % Loop through controls
 for ctrl_index = 1:num_plots
@@ -51,7 +54,7 @@ for ctrl_index = 1:num_plots
         nrm = ((x1 - theta(1)).^2 + (x2 - theta(2)).^2).^(0.5);
 
         % Calculate value in summation: e^{-\| (x_t + \Delat t f(x_t,u_t)) - \theta \|_2}
-        val = exp(1).^(-1.*nrm);
+        val = exp(-1.*nrm); %exp(1).^(-1.*nrm);
 
         % Add to running value of summation
         intControls = intControls + inc*val;
@@ -64,18 +67,34 @@ for ctrl_index = 1:num_plots
     x2 = x{2} + deltaT .* v .* sin(u);
 
     nrm = ((x1 - theta(1)).^2 + (x2 - theta(2)).^2).^(0.5);
-    val = exp(1).^(-1.*nrm);
+    val = exp(-1.*nrm); %exp(1).^(-1.*nrm);
 
     prob_u = val ./ intControls;
     
+    % NOTE: The prob_u is actually repeated for each 3rd index 
+    %   so we can take any of the entries for plotting purposes
+    %  (to see this check out a 5x5x5 grid and look at what prob_u looks
+    %  like. This is beacuse its not actually corresponding to different
+    %  posteriors, since we are only trying to plot our observation model's
+    %  results, P(u | x, theta) and not the posterior P(theta | u, x). 
+    
     % Should this be transposed to corresponed to x-y grid? If so, then
     % graphs make sense
-    prob_u_high = prob_u(:,:,38).'; % high posterior prob of ~1.0
+    % prob_u_high = prob_u(:,:,end); % high posterior prob of ~1.0
     
     %% Plot
     subplot(2,3,ind)
-    heatmap(prob_u_high);
+    
+    % Generate pseudocolor map of probabilities of u at each state in grid.
+    h = pcolor(grid2D.xs{1}, grid2D.xs{2}, prob_u(:,:,1));
+    colorbar
+    caxis([0,0.5]);
+    set(h, 'EdgeColor', 'none');
     title(["u = " u])
+    
+    hold on 
+    % Plot the goal location.
+    scatter([theta(1)], [theta(2)], 25, 'r', 'filled');
     
     ind = ind + 1;
 end
