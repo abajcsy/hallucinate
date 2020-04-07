8d43d1b94ca3d4dafc54c44e516a24da22642159
Anand <anandsranjan@gmail.com>
2020-04-07 10:07:57 +1000

    Added BoltzmannTwoGoal

---
+++ matlab/predictors/human_models/hams/boltzmannTwoGoalHuman_ham.m
@@ -0,0 +1,77 @@
+function hamValue = boltzmannTwoGoalHuman_ham(t, data, deriv, schemeData)
+
+if ~iscell(deriv)
+  deriv = num2cell(deriv);
+end
+
+% Grab the GaussianHuman so we can access its parameters.
+human = schemeData.dynSys;
+x = schemeData.grid.xs;
+uMode = schemeData.uMode;
+
+%  Compute the Hamiltonian over all states and discretized controls.
+p1 = repmat(deriv{1}, [1, 1, 1, human.numCtrls]);
+p2 = repmat(deriv{2}, [1, 1, 1, human.numCtrls]);
+p3 = repmat(deriv{3}, [1, 1, 1, human.numCtrls]);
+pdot_f = p1 .* human.xdot{1} + p2 .* human.xdot{2} + p3 .* human.xdot{3};
+
+% Maximize/minimize Hamiltonian.
+if strcmp(uMode, 'min')
+	hamValue = min(pdot_f, [], 4);
+else
+	hamValue = max(pdot_f, [], 4);
+end
+
+% Negate hamValue if backward reachable set.
+if strcmp(schemeData.tMode, 'backward')
+   hamValue = -hamValue;
+end
+
+end
+% 
+% function hamValue = boltzmannHuman_ham(t, data, deriv, schemeData)
+% 
+% if ~iscell(deriv)
+%   deriv = num2cell(deriv);
+% end
+% 
+% % Grab the GaussianHuman so we can access its parameters.
+% human = schemeData.dynSys;
+% x = schemeData.grid.xs;
+% uMode = schemeData.uMode;
+% 
+% % %  Compute the Hamiltonian over all states and discretized controls.
+% % p1 = repmat(deriv{1}, [1, 1, 1, human.numCtrls]);
+% % p2 = repmat(deriv{2}, [1, 1, 1, human.numCtrls]);
+% % p3 = repmat(deriv{3}, [1, 1, 1, human.numCtrls]);
+% % pdot_f = p1.*human.xdot{1} + p2.*human.xdot{2} + p3.*human.xdot{3};
+% % 
+% % % Maximize/minimize Hamiltonian
+% % if strcmp(uMode, 'min')
+% % 	hamValue = min(pdot_f, [], 4);
+% % else
+% % 	hamValue = max(pdot_f, [], 4);
+% % end
+% 
+% hamU = human.likelyCtrls{1};
+% xdot = human.dynamics(1,x,hamU);
+% hamValue = min(hamValue, hamU);
+% 
+% for i=2:human.numCtrls
+%     % Get dynamics at each state given current control.
+%     %xdot = human.xdot{i};
+%     u = human.likelyCtrls{i};
+%     xdot = human.dynamics(1,x,u);
+%     
+%     % Compute Hamiltonian: 
+%     %   \grad_x V(x,t) * f(x,u).
+%     hamU = deriv{1} .* xdot{1} + deriv{2} .* xdot{2} + deriv{3} .* xdot{3};
+%     
+%     % Maintain the min or max Hamiltonian value.
+%     if strcmp(uMode, 'max')
+%         hamValue = max(hamValue, hamU);
+%     else
+%         hamValue = min(hamValue, hamU);
+%     end
+% end
+% end
